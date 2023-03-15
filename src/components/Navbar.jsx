44bc1fdd3e13ebc92ae45a0aee91af5d90a1a298
prati <pratima.sampath@gmail.com>
2023-03-15 15:19:29 +0100

    tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {ShoppingCartIcon} from '@heroicons/react/24/solid'
 
 const Navbar = () => {
   return (
-    <div>
-      <ul class="flex">
-        <li class="mr-6">
-          < Link class="text-blue-500 hover:text-blue-800" to={"/"}>Shop</Link>
+    <div className="flex justify-between">
+      <div>
+        <span>shopping</span>
+      </div>
+      <div>
+      <ul className="flex">
+        <li className="mr-6">
+          <Link className="text-blue-500 hover:text-blue-800" to={"/"}>Shop</Link>
         </li>
-        <li class="mr-6">
-          <Link class="text-blue-500 hover:text-blue-800" href="#">Cart</Link>
+        <li className="mr-6 mr-1">
+          <Link className="text-blue-500 hover:text-blue-800" to={"/cart"}>Cart</Link>
         </li>
-        <li class="mr-6">
-          <Link class="text-blue-500 hover:text-blue-800" href="#">Link</Link>
+        <li className="mt-2 mr-6" >
+        <ShoppingCartIcon className="h-3 w-3 text-blue-500"/>
         </li>
-        <li class="mr-6">
-          <Link class="text-gray-400 cursor-not-allowed" href="#">Disabled</Link>
-        </li>
+        
       </ul>
     </div>
+    </div>
+    
   )
 }
 
