0150f871ad0832e0963dc3556289e0572fcaa1d0
prati <pratima.sampath@gmail.com>
2023-03-28 15:42:42 +0200

    finished useContex and useReducer and working properly

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useContext, useState} from 'react'
 import {
     Card,
     CardHeader,
@@ -7,13 +7,45 @@ import {
     Typography
 } from "@material-tailwind/react";
 import Rating from './Rating';
+import { ReducerContext } from '../context/Reducer';
 // import { CartState } from '../context/Context';
 // import Cart from '../pages/Cart';
 
 const SingleProduct = ({ prod }) => {
+    const[quantity,setQuantity] = useState(0)
+    const { state, dispatch  } = useContext(ReducerContext)
 
-    const quantity = 1
+    const AddToCart = () => {
+        dispatch({
+            type: "ADD_TO_CART",
+            payload: prod,
+          })
+          setQuantity(quantity+1)
+          
+    }
+    
+    const RemoveFromCart = () => {
+        dispatch({
+            type: "REMOVE_FROM_CART",
+            payload: prod,
+          })
+          setQuantity(0)
+    }
 
+    const updateCart = () => {
+        dispatch({
+            type: "UPDATE_CART",
+            payload: prod,
+          })
+          setQuantity(quantity+1)
+    }
+    const reduceFromCart = () => {
+        dispatch({
+            type: "REDUCE_FROM_CART",
+            payload: prod,
+          })
+          setQuantity(quantity-1)
+    }
     return (
         <Card className="w-60 m-4 overflow-hidden shadow-lg rounded-none ">
             <CardHeader floated={false} className="h-40 rounded-none">
@@ -30,29 +62,27 @@ const SingleProduct = ({ prod }) => {
                    {prod.fastDelivery?(<span>Fast Delivery</span>):(<span>4 day Delivery</span>)}
                    <Rating rating={prod.ratings}/>
                 </Typography>
-                {/* <Typography color="blue" className="font-medium" textGradient>
-                </Typography> */}
             </CardBody>
             <CardFooter className="flex justify-evenly  gap-7 pt-1">
                 {/* <Tooltip content="Like">
                 </Tooltip> */}
                 <div>
                 {quantity===0? (
-                    <button disabled={!prod.inStock} className=' border-solid p-1 bg-cyan-600'>
+                    <button disabled={!prod.inStock} onClick={AddToCart} className=' border-solid p-1 bg-cyan-600'>
                     {!prod.inStock?"out of stock":" Add to cart"}</button>
                     ): (<div className=' justify-centre'>
                             <div className='flex justify-centre'>
-                            <button  className=' border-solid p-1 bg-cyan-600'>
+                            <button onClick={reduceFromCart} className=' border-solid p-1 bg-cyan-600'>
                                 -
                             </button>
                             <div className='m-1'>
                                 {quantity} in cart
                             </div>
-                            <button  className=' border-solid p-1 bg-cyan-600'>
+                            <button onClick={updateCart} className=' border-solid p-1 bg-cyan-600'>
                                 +
                             </button>
                             </div>
-                            <button className=' border-solid p-1 mt-2 bg-red-600'>
+                            <button onClick={RemoveFromCart} className=' border-solid p-1 mt-2 bg-red-600'>
                                 Remove Item
                             </button>
                         </div>)
@@ -65,24 +95,4 @@ const SingleProduct = ({ prod }) => {
     )
 }
 
-
 export default SingleProduct
-// {/* <CardFooter className="flex justify-evenly  gap-7 pt-1"> */}
-// {/* <Tooltip content="Like">
-// </Tooltip> */}
-// {/* {
-//     cart.some((p) => p.id === prod.id) ? (
-        // <button onClick = { () =>
-
-        //    { dispatch({type:'RemoveCart',payload:prod})
-        //         console.log('test', cart)}} 
-        //   className=' border-solid p-1 bg-red-600'>Remove Item</button>
-
-//     ): (
-
-//         <button onClick = {handleClick} disabled={!prod.inStock} className=' border-solid p-1 bg-cyan-600'>
-//         {!prod.inStock?"out of stock":" Add to cart"}
-    
-//         </button>
-//     )
-// }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import {ReducerContext} from '../context/Reducer'
+import SingleCart from './SingleCart';
 
 const Cart = () => {
+  const { state, dispatch  } = useContext(ReducerContext)
+  console.log(state,"state from cart")
+  
+  
   return (
-    <div>
-      CART
-    </div>
+    <div className='flex flex-row flex-wrap  md:flex-1 justify-evenly' >
+
+      {/* <article className="mt-5 grid grid-cols-3 gap-10"> */}
+      {state.cart.map((prod) => {
+       return (        
+        <SingleCart prod ={prod} key={prod.id}/>
+        )
+      })}
+      </div>
   )
 }
 
